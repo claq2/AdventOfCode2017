3a36ca4373d6174399cc0adc0d9d1c64d738709c
James McLachlan <claquer@gmail.com>
2017-12-07 09:27:25 -0500

    Solved!

--- cheppers/index.js
+++ cheppers/index.js
@@ -13,12 +13,20 @@ function getInt64Bytes( x ){
 
 let space = ' '.charCodeAt(0);
 console.log('space', space);
+var upBuf = Buffer.from('↑');
+console.log('upBuf', upBuf);
 let up = '↑'.charCodeAt(0);
 let down = '↓'.charCodeAt(0);
 let left = '←'.charCodeAt(0);
 let right = '→'.charCodeAt(0);
 let b = 'B'.charCodeAt(0);
 let a = 'A'.charCodeAt(0);
+let k = 'k'.charCodeAt(0);
+let o = 'o'.charCodeAt(0);
+let n = 'n'.charCodeAt(0);
+let al = 'a'.charCodeAt(0);
+let m = 'm'.charCodeAt(0);
+let i = 'i'.charCodeAt(0);
 let upBytes = [parseInt('e2', 16), parseInt('86', 16), parseInt('91', 16)];//getInt64Bytes(up); [38]
 let downBytes = [parseInt('e2', 16), parseInt('86', 16), parseInt('93', 16)];//getInt64Bytes(down); [40]
 let leftBytes = [parseInt('e2', 16), parseInt('86', 16), parseInt('90', 16)];//getInt64Bytes(left); [37]
@@ -38,9 +46,15 @@ console.log('rightBytes', rightBytes);
 // key.push(right);
 let key = [];
 //key.push(space);
-key = key.concat(upBytes).concat(upBytes).concat(downBytes).concat(downBytes).concat(leftBytes).concat(rightBytes).concat(leftBytes).concat(rightBytes);//.push(b).push(a);
-key.push(b);
-key.push(a);
+//key = key.concat(upBytes).concat(upBytes).concat(downBytes).concat(downBytes).concat(leftBytes).concat(rightBytes).concat(leftBytes).concat(rightBytes);//.push(b).push(a);
+//key.push(b);
+//key.push(a);
+key.push(k);
+key.push(o);
+key.push(n);
+key.push(al);
+key.push(m);
+key.push(i);
 console.log('key', key);
 console.log('key length', key.length);
 
@@ -55,7 +69,7 @@ for (let i = 0; i < buf.length; i++) {
     //console.log('key element', i % key.length);
     xored.push(element ^ key[i % key.length]);
 }
-console.log('xored', xored);
+//console.log('xored', xored);
 console.log('xored', xored.map(i=>i.toString(16)));
 let xoredBase64 = Buffer.from(xored).toString('base64');
 console.log('xoredBase64', xoredBase64);
