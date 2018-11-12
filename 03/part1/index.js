5d719d9df69df397585319aea0c608f674ce1b17
James McLachlan <claquer@gmail.com>
2018-11-12 14:32:44 -0500

    A couple more tests

--- 03/part1/index.js
+++ 03/part1/index.js
@@ -34,6 +34,8 @@ console.log(`Dim 2: ${dim(2)}`);
 console.log(`Dim 3: ${dim(3)}`);
 console.log(`Dim 4: ${dim(4)}`);
 console.log(`Dim 5: ${dim(5)}`);
+console.log(`Dim 6: ${dim(6)}`);
+console.log(`Dim 590: ${dim(590)}`);
 console.log(`Number 12 is in dim ${findDim(12)}`);
 console.log(`Number 18 is in dim ${findDim(18)}`);
 console.log(`Number 347991 is in dim ${findDim(347991)}`);
